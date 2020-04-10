477cbb452d4ffada0334eb3cc72c3fd39eda9e14
Macray <Macray607@hotmail.com>
2020-04-10 14:05:40 -0500

    Added route and added context to view

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   root to:'drink#index'
   resources :drink, only: :show
+  get 'country_of_origin/:id', to: 'country_of_origin#show', as: 'country_of_origin'
   get 'pages/index'
   get '/p/:permalink', to: 'pages#permalink', as: "permalink"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
